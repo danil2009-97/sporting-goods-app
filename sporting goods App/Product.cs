202d66dc86574eb1c4c5bb30b386be356b90b269
Попов Даниил <danil2009-97@mail.ru>
2017-03-13 23:29:06 +0300

    Adding Classes and Fields

---
+++ sporting goods App/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sporting_goods_App
+{
+    class Product
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public Category Category { get; set; }
+
+        public Product(string name, string description, Category category)
+        {
+            Name = name;
+            Description = description;
+            Category = category;
+        }
+    }
+}
---
+++ sporting goods App/Shop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sporting_goods_App
+{
+    class Shop
+    {
+        public List<Product> Products { get; set; }
+
+        public string Address { get; set; }
+
+        public Shop( List<Product>products, string address)
+        {
+            Products = products;
+            Address = address;
+        }
+    }
+}
